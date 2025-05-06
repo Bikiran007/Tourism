c02e67bb2328a8081cc5f358eb872743a2e46fdf
Bhavuk Saluja <bhavuksaluja6@gmail.com>
2025-05-06 21:27:53 +0530

    Update bhavuk.js
    adding js

--- bhavuk.js
+++ bhavuk.js
@@ -1,1 +1,8 @@
+let current = 0;
+const images = document.querySelectorAll('#slider img');
 
+function showNextImage() {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}
